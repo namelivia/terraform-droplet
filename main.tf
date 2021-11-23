e55f52bdd8fb8d756496ed44217f0d35665460ee
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-11-23 18:58:19 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+resource "digitalocean_ssh_key" "deployer" {
+  name   = "deployer-key"
+  public_key = "${var.ssh_key}"
+}
+
+resource "digitalocean_droplet" "namelivia" {
+  image  = "ubuntu-18-04-x64"
+  name   = "namelivia"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [digitalocean_ssh_key.deployer.fingerprint]
+}
+
+output "ip" {
+  value = digitalocean_droplet.namelivia.ipv4_address
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "do_token" {
+  type = string
+  description = "Token for Digital Ocean"
+}
+
+variable "ssh_key" {
+  type = string
+  description = "SSH Key to access the instance"
+}
