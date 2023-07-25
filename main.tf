c9bdab4113fa343356b927ce36ed3b291011de10
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-07-25 13:46:47 +0200

    Add firewall

--- main.tf
+++ main.tf
@@ -19,3 +19,37 @@ resource "digitalocean_droplet" "namelivia" {
   size   = "s-1vcpu-1gb"
   ssh_keys = [digitalocean_ssh_key.deployer.fingerprint]
 }
+
+resource "digitalocean_firewall" "namelivia" {
+  name = "only-22"
+
+  droplet_ids = [digitalocean_droplet.namelivia.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
